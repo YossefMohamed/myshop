824aa0789888558d4304c61e5983a96973b65f66
YossefMohamed <youssef171035@feng.bu.edu.eg>
2021-01-17 19:06:26 +0200

    Finish Cart Component

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const dotenv = require("dotenv");
-
+app.use(express.json());
 app.use(cors());
 const productRouter = require("./routes/productRoutes");
 const connectDB = require("./database");
